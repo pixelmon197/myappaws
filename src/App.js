1784c4527a21160d85358013e9ec4f8bb3698d07
pixelmon197 <pixelmon197@gmail.com>
2025-02-12 23:59:02 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          I miss you Shakira Guadalupe, return plis
+          I miss you osorio, return plis
         </p>
       </header>
     </div>
