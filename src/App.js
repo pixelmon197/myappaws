5c2082582b0a509df15ad10a7076e7c1607ece50
pixelmon197 <pixelmon197@gmail.com>
2025-02-12 23:40:31 -0600

    subiendo otra vez

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          I miss you Shakira Guadalupe, return plis
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
